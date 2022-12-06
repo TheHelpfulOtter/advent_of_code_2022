19807ed90d39754cee4d03e9674ed06d11d30cf9
Daniel Kuan <dkuan0808@outlook.com>
2022-12-06 16:41:53 +0300

    Add code for day 2 part 1

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -5,37 +5,60 @@ The score for a single round is the score for the shape you selected (1 for Rock
 plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
 """
 
-player_1 = {"A": "Rock", "B": "Paper", "C": "Scissors"}
-
-player_2 = {"X": "Rock", "Y": "Paper", "Z": "Scissors"}
-
-rounds = [
-    [player_1["A"], player_2["Y"]],
-    [player_1["B"], player_2["X"]],
-    [player_1["C"], player_2["Z"]],
-]
-
+player_1_moves = {"A": "Rock", "B": "Paper", "C": "Scissors"}
+player_2_moves = {"X": "Rock", "Y": "Paper", "Z": "Scissors"}
 player_1_score = 0
 player_2_score = 0
 
-for i in range(len(rounds)):
-    # Get score for move choice
-    if rounds[i][0] == "Rock":
+file_object = open("day_2_puzzle_input.txt", "r")
+file_data = file_object.read()
+file_data = file_data.split("\n")
+file_data = [x.split(" ") for x in file_data]
+
+for i in range(len(file_data)):
+    p1_move = player_1_moves[file_data[i][0]]
+    p2_move = player_2_moves[file_data[i][1]]
+
+    ## Get points for making a move
+    # Player 1
+    if p1_move == "Rock":
         player_1_score += 1
-    elif rounds[i][0] == "Paper":
+    elif p1_move == "Paper":
         player_1_score += 2
-    elif rounds[i][0] == "Scissors":
+    elif p1_move == "Scissors":
         player_1_score += 3
 
-    if rounds[i][1] == "Rock":
+    # Player 2
+    if p2_move == "Rock":
         player_2_score += 1
-    elif rounds[i][1] == "Paper":
+    elif p2_move == "Paper":
         player_2_score += 2
-    elif rounds[i][1] == "Scissors":
+    elif p2_move == "Scissors":
         player_2_score += 3
 
-    # Get win loss score
+    ## Scoring
+    if p1_move == p2_move:
+        player_1_score += 3
+        player_2_score += 3
+    elif p1_move == "Rock" and p2_move == "Paper":
+        player_1_score += 0
+        player_2_score += 6
+    elif p1_move == "Paper" and p2_move == "Scissors":
+        player_1_score += 0
+        player_2_score += 6
+    elif p1_move == "Scissors" and p2_move == "Rock":
+        player_1_score += 0
+        player_2_score += 6
+    elif p1_move == "Rock" and p2_move == "Scissors":
+        player_1_score += 6
+        player_2_score += 0
+    elif p1_move == "Paper" and p2_move == "Rock":
+        player_1_score += 6
+        player_2_score += 0
+    elif p1_move == "Scissors" and p2_move == "Paper":
+        player_1_score += 6
+        player_2_score += 0
 
 
-print(player_1_score)
-print(player_2_score)
+print(f"Player 1 score: {player_1_score}")
+print(f"Player 2 score: {player_2_score}")
