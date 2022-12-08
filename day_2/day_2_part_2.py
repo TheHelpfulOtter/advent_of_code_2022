6b299d1e0f531590f45899ac0ac70ac0ab094af2
Daniel Kuan <dkuan0808@outlook.com>
2022-12-08 16:40:03 +0300

    Add code for day2 part 2

--- day_2/day_2_part_2.py
+++ day_2/day_2_part_2.py
@@ -2,28 +2,20 @@
 https://adventofcode.com/2022/day/2#part2
 
 X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+Ans 12683
+
+current 12836
+
+Very confusingly phrased question:
+Basically, if the second column is X, you have to lose. Y, you have to draw, Z, you have to win
+
+This means that you need to pick the piece that will cause the win, draw, or loss. REGARDLESS of what was said in part 1
+e.g X = rock, Y = paper, Z = scissors
+throw it away
+
+So if the opponent chooses rock, and you have a Z, this means you need to win. So to win, you have to choose 
 """
-
-
-# player_1_moves = {"A": "Rock", "B": "Paper", "C": "Scissors"}
-# player_2_moves = {"X": "Rock", "Y": "Paper", "Z": "Scissors"}
-# #                 Rock = LOSE   Paper = DRAW  Scissors = WIN
-
-# Player 2's perspective
-# game_moves = {
-#     "A X": "draw",
-#     "A Y": "win",
-#     "A Z": "lose",
-#     "B X": "lose",
-#     "B Y": "draw",
-#     "B Z": "win",
-#     "C X": "win",
-#     "C Y": "lose",
-#     "C Z": "draw",
-# }
-
-
-# file_data = [x.split(" ") for x in file_data]
 
 
 def calc_score():
@@ -32,91 +24,36 @@ def calc_score():
     file_data = file_data.split("\n")
 
     player_2_score = 0
-    i = 0
 
     # Calculate the score
     for move in file_data:
-        i += 1
-        # Draw
-        if move in ("A X", "B Y", "C Z"):
-            print("DRAW Added 3 points")
-            player_2_score += 3
-        # Win
-        elif move in ("A Y", "B Z", "C X"):
-            print("WIN Added 6 points")
-            player_2_score += 6
-        # Lose
-        elif move in ("A Z", "B X", "C Y"):
-            print("LOSE Added 0 points")
-            player_2_score += 0
-
-        # Chose rock
+        # X is Lose
         if "X" in move:
-            print("ROCK Added 1 point")
-            player_2_score += 1
-        # Chose paper
-        elif "Y" in move:
-            print("PAPER Added 2 points")
-            player_2_score += 2
-        # Chose scissors
-        elif "Z" in move:
-            print("SCISSORS Added 3 points")
-            player_2_score += 3
-
-        print("===================================================")
-        print(f"Current score is: {player_2_score } for move {i + 1}")
-        print("===================================================")
+            if move == ("A X"):  # Opp. chose rock, so you must choose scissors
+                player_2_score += 0 + 3
+            elif move == ("B X"):  # Opp. chose paper, so you must choose rock
+                player_2_score += 0 + 1
+            elif move == ("C X"):  # Opp. chose scissors, so you must choose paper
+                player_2_score += 0 + 2
+        # Y is draw
+        if "Y" in move:
+            if move == ("A Y"):
+                player_2_score += 3 + 1
+            elif move == ("B Y"):
+                player_2_score += 3 + 2
+            elif move == ("C Y"):
+                player_2_score += 3 + 3
+        # Z is win
+        if "Z" in move:
+            if move == ("A Z"):
+                player_2_score += 6 + 2
+            elif move == ("B Z"):
+                player_2_score += 6 + 3
+            elif move == ("C Z"):
+                player_2_score += 6 + 1
 
     return player_2_score
 
 
 total_score = calc_score()
 print(total_score)
-
-# for i in range(len(file_data)):
-#     p1_move = player_1_moves[file_data[i][0]]
-#     p2_move = player_2_moves[file_data[i][1]]
-
-
-#     ## Get points for making a move
-#     # Player 1
-#     if p1_move == "Rock":
-#         player_1_score += 1
-#     elif p1_move == "Paper":
-#         player_1_score += 2
-#     elif p1_move == "Scissors":
-#         player_1_score += 3
-
-#     # Player 2
-#     if p2_move == "Rock":
-#         player_2_score += 1
-#     elif p2_move == "Paper":
-#         player_2_score += 2
-#     elif p2_move == "Scissors":
-#         player_2_score += 3
-
-#     ## Scoring
-#     if p2_move == "Paper":  # p2 Paper = DRAW
-#         player_1_score += 3
-#         player_2_score += 3
-#     elif p2_move == "Scissors":  # p2 Scissors = WIN
-#         player_1_score += 0
-#         player_2_score += 6
-#     elif p2_move == "Rock":  # p2 Rock = LOSE
-#         player_1_score += 6
-#         player_2_score += 0
-
-
-# print(f"Player 1 score: {player_1_score}")
-# print(f"Player 2 score: {player_2_score}")
-
-# for player_2_move in game_moves:
-#     # Draw
-#     if move in ("A X", "B Y", "C Z"):
-#         player_2_score += 3
-#     # Win
-#     elif move in player_2_move == "A Y" or "B Z" or "C X":
-#         player_2_score += 6
-#     # Lose
-#     elif move == player_2_move == "A Z" or "B X" or "C Y":
-#         player_1_score += 6
